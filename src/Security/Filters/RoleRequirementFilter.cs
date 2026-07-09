cb4e627f8d3214aa8233776b98eceedbf671e1ea
Artur Rios <arturdev@duck.com>
2026-07-09 15:13:41 -0300

    fix: RoleRequirementFilter honors [AllowAnonymous]

--- src/Security/Filters/RoleRequirementFilter.cs
+++ src/Security/Filters/RoleRequirementFilter.cs
@@ -1,6 +1,7 @@
 ﻿using ArturRios.Extensions;
 using ArturRios.Output;
 using ArturRios.Util.Http;
+using ArturRios.Util.WebApi.Security.Attributes;
 using ArturRios.Util.WebApi.Security.Records;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -8,7 +9,8 @@ using Microsoft.AspNetCore.Mvc.Filters;
 namespace ArturRios.Util.WebApi.Security.Filters;
 
 /// <summary>Authorization filter that rejects requests with a 403 response unless the authenticated user's role
-/// is one of <paramref name="authorizedRoles"/>. Applied declaratively via <see cref="Attributes.RoleRequirementAttribute"/>.</summary>
+/// is one of <paramref name="authorizedRoles"/>. Applied declaratively via <see cref="Attributes.RoleRequirementAttribute"/>,
+/// unless the action is marked with <see cref="AllowAnonymousAttribute"/>.</summary>
 /// <param name="authorizedRoles">The role values permitted to access the resource.</param>
 public class RoleRequirementFilter(params int[] authorizedRoles) : IAuthorizationFilter
 {
@@ -16,6 +18,13 @@ public class RoleRequirementFilter(params int[] authorizedRoles) : IAuthorizatio
     /// <param name="context">The authorization filter context for the current request.</param>
     public void OnAuthorization(AuthorizationFilterContext context)
     {
+        var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
+
+        if (allowAnonymous)
+        {
+            return;
+        }
+
         var user = context.HttpContext.Items["User"] as AuthenticatedUser;
 
         var authorized = false;
