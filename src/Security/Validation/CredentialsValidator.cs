2116c68efbd7570db98b593ac2761909f5be34c1
Artur Rios <arturdev@duck.com>
2026-07-09 14:32:22 -0300

    feat: validate email format and password length in CredentialsValidator

--- src/Security/Validation/CredentialsValidator.cs
+++ src/Security/Validation/CredentialsValidator.cs
@@ -7,7 +7,7 @@ public class CredentialsValidator : AbstractValidator<Credentials>
 {
     public CredentialsValidator()
     {
-        RuleFor(authCredentials => authCredentials.Email).NotEmpty();
-        RuleFor(authCredentials => authCredentials.Password).NotEmpty();
+        RuleFor(credentials => credentials.Email).NotEmpty().EmailAddress();
+        RuleFor(credentials => credentials.Password).NotEmpty().MinimumLength(8);
     }
 }
